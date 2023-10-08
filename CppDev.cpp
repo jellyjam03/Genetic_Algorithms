941385125f06c51fc84f8c8273f6e4f091c11ae4
MediocreKarma <103333398+MediocreKarma@users.noreply.github.com>
2023-10-08 23:44:26 +0300

    numele res trebuie schimbat

--- CppDev.cpp
+++ CppDev.cpp
@@ -6,55 +6,55 @@
 #include <random>
 #include <iomanip>
 #include <chrono>
+#include <fstream>
+#include <future>
 
 constexpr double M_PI = 3.14159265358979323846;
 constexpr double SMALLEST_INCREMENT = 1e-5;
 
 double michalewicz(const std::vector<double>& x) {
     auto michalewicz1Dim = [](double x, int i) -> double {
-        return sin(x) * std::pow(sin((x * x) * (i + 1) / M_PI), 20);
+        return sin(x) * std::pow(sin((x * x) * i / M_PI), 20);
     };
-
     double sum = 0;
     for (size_t i = 0; i < x.size(); ++i) {
-        sum += michalewicz1Dim(x[i], i);
+        sum += michalewicz1Dim(x[i], (int)i + 1);
     }
     return -sum;
 }
 
 double rastrigin(const std::vector<double>& x) {
-    auto rastrigin1Dim = [](double x) -> double {
-        return x * x - 10 * cos(2 * M_PI * x);
+    auto rastrigin1Dim = [](double sum, double x) -> double {
+        return sum + x * x - 10 * cos(2 * M_PI * x);
     };
-    double sum = 0;
-    for (double elem : x) {
-        sum += rastrigin1Dim(elem);
-    }
-    return 10 * x.size() + sum;
+    return 10 * x.size() + std::accumulate(x.begin(), x.end(), 0.0, rastrigin1Dim);
 }
 
-double customFunc(const std::vector<double>& x) {
-    auto customFunc1Dim = [](double x) -> double {
-        return sin(2 * M_PI * x);
+double sphere(const std::vector<double>& x) {
+    auto sphere1DimAux = [](double sum, double x) -> double {
+        return sum + x * x;
     };
-    double sum = 0;
-    for (double elem : x) {
-        sum += customFunc1Dim(elem);
-    }
-    return sum;
+    return std::accumulate(x.begin(), x.end(), 0.0, sphere1DimAux);
 }
 
 double shubert(const std::vector<double>& x) {
-    auto customFunc1Dim = [](double x) -> double {
-        double sum = 0;
+    auto customFunc1Dim = [](double sumSoFar, double x) -> double {
+        double sumCurrent = 0;
         for (int i = 1; i <= 5; ++i) {
-            sum += i * cos((i + 1) * x + i);
+            sumCurrent += i * cos((i + 1) * x + i);
         }
-        return sum;
+        return sumSoFar + sumCurrent;
+    };
+    return std::accumulate(x.begin(), x.end(), 0.0, customFunc1Dim);
+}
+
+double sumSquares(const std::vector<double>& x) {
+    auto sumSquares1Dim = [](double x, int i) -> double {
+        return i * x * x;
     };
     double sum = 0;
-    for (double elem : x) {
-        sum += customFunc1Dim(elem);
+    for (size_t i = 0; i < x.size(); ++i) {
+        sum += sumSquares1Dim(x[i], (int)i + 1);
     }
     return sum;
 }
@@ -63,26 +63,35 @@ struct Function {
 private: Function();
     
 public:
-    Function(std::function<double(const std::vector<double>&)> func_, const std::string& name_) :
-        func(func_), name(name_) {}
+    Function(std::function<double(const std::vector<double>&)> func_, const std::string& name_, double lo_, double hi_) :
+        func(func_), name(name_), lo(lo_), hi(hi_) {}
 
     std::function<double(const std::vector<double>&)> func;
-    const std::string& name;
+    std::string name;
+    double lo;
+    double hi; // inclusive range end
 
-    double operator () (const std::vector<double>& x) {
+    double operator () (const std::vector<double>& x) const {
         return func(x);
     }
 };
 
-void euristicGlobalMinimum(Function func, int dimensions, double rangeStart, double rangeEnd) {
-    std::cout << func.name << " with " << dimensions << " dimensions and range [" << rangeStart << ", " << rangeEnd << "]\n";
+std::string heuristicGlobalMinimum(const Function& func, int dimensions) {
+    using namespace std::chrono;
+    auto start = high_resolution_clock::now();
+    double rangeStart = func.lo;
+    double rangeEnd = func.hi;
+    std::stringstream prettyOutput;
+    std::stringstream simpleOutput;
+    simpleOutput << std::fixed << std::setprecision(5);
+    simpleOutput << func.name << ',' << dimensions << ',' << rangeStart << ',' << rangeEnd << ',';
     const double INCREMENTER[] = { 0, -SMALLEST_INCREMENT, SMALLEST_INCREMENT };
     const double ERROR = 1e-6;
     std::vector<double> bestX(dimensions);
 
-    std::random_device rd;
-    std::mt19937 rng(rd());
-    std::uniform_real_distribution<double> urd(rangeStart, rangeEnd + SMALLEST_INCREMENT);
+    static std::random_device rd;
+    static std::mt19937 rng(rd());
+    std::uniform_real_distribution<double> urd(rangeStart, rangeEnd + ERROR);
 
     std::vector<int> divOffsets(dimensions);
     divOffsets[0] = 1;
@@ -94,11 +103,15 @@ void euristicGlobalMinimum(Function func, int dimensions, double rangeStart, dou
         elem = std::round(urd(rng) / SMALLEST_INCREMENT) * SMALLEST_INCREMENT;
     }
     double bestValue = func(bestX);
+    std::vector<double> x(bestX.size());
+    int loop = 0;
     while (true) {
         double bestNewValue = bestValue;
         int bestNeighbour = 0;
         for (int neighbour = 1, NEIGHBOURS = pow(3, dimensions); neighbour < NEIGHBOURS; ++neighbour) {
-            std::vector<double> x(bestX);
+            for (size_t elemIndex = 0; elemIndex < bestX.size(); ++elemIndex) {
+                x[elemIndex] = bestX[elemIndex];
+            }
             bool ok = true;
             for (size_t i = 0; i < x.size(); ++i) {
                 x[i] += INCREMENTER[(int)(neighbour / divOffsets[i]) % 3];
@@ -119,36 +132,53 @@ void euristicGlobalMinimum(Function func, int dimensions, double rangeStart, dou
         if (bestNeighbour == 0) {
             break;
         }
+        // around 250'000'000 steps
+        if (dimensions == 10 && ++loop == 4000) {
+            loop = 0;
+            std::cout << "heuristic " << func.name << " still searching\n";
+        }
         bestValue = bestNewValue;
         for (size_t i = 0; i < bestX.size(); ++i) {
             bestX[i] += INCREMENTER[(int)(bestNeighbour / divOffsets[i]) % 3];
         }
     }
-    std::cout << "Found the value " << bestValue << " for x = {";
-    for (double elem : bestX) {
-        std::cout << elem << ", ";
+    simpleOutput << bestValue << ",{";
+    for (size_t i = 0; i < bestX.size() - 1; ++i) {
+        simpleOutput << bestX[i] << ' ';
     }
-    std::cout << "\b\b} using the euristic method\n\n";
+    simpleOutput << bestX.back() << "},heuristic,";
+    auto stop = high_resolution_clock::now();
+    simpleOutput << duration_cast<milliseconds>(stop - start).count();
+    return simpleOutput.str();
 }
 
-void deterministicGlobalMinimum(Function func, int dimensions, double rangeStart, double rangeEnd) {
-    std::cout << func.name << " with " << dimensions << " dimensions and range [" << rangeStart << ", " << rangeEnd << "]\n";
+std::string deterministicGlobalMinimum(const Function& func, int dimensions) {
+    using namespace std::chrono;
+    auto start = high_resolution_clock::now();
+    double rangeStart = func.lo;
+    double rangeEnd = func.hi;
+    std::stringstream simpleOutput;
+    simpleOutput << std::fixed << std::setprecision(5);
+    simpleOutput << func.name << ',' << dimensions << ',' << rangeStart << ',' << rangeEnd << ',';
     rangeEnd += SMALLEST_INCREMENT;
     size_t stepsPerDimension = std::ceil((rangeEnd - rangeStart) / SMALLEST_INCREMENT);
     if (stepsPerDimension >= pow(SIZE_MAX, 1. / dimensions)) {
-        std::cout << "Step count over size_t limit, iteration is impossible and computation is unfeasable\n";
-        std::cout << "Reduce dimension count or range size (current step count is " << pow(stepsPerDimension, dimensions) << ")\n\n";
-        return;
+        simpleOutput << "NULL,NULL,deterministic,NULL";
+        return simpleOutput.str();
     }
     std::vector<size_t> divOffsets(dimensions);
     divOffsets[0] = 1;
     for (size_t i = 1; i < dimensions; ++i) {
         divOffsets[i] = divOffsets[i - 1] * stepsPerDimension;
     }
-    double bestValue = func(std::vector<double>(dimensions, rangeStart));
+    std::vector<double> x(dimensions, rangeStart);
+    double bestValue = func(x);
     size_t bestStep = 0;
-    for (size_t step = 0, steps = pow(stepsPerDimension, dimensions); step < steps; ++step) {
-        std::vector<double> x(dimensions, rangeStart);
+    for (size_t step = 1, steps = pow(stepsPerDimension, dimensions); step < steps; ++step) {
+        if (step % 250'000'000 == 0) {
+            std::cout << "deterministic " << func.name << " is at " << (100.0 * step / steps) << "%\n";
+        }
+        std::fill(x.begin(), x.end(), rangeStart);
         for (size_t i = 0; i < dimensions; ++i) {
             x[i] += SMALLEST_INCREMENT * (step / divOffsets[i] % stepsPerDimension);
         }
@@ -162,18 +192,51 @@ void deterministicGlobalMinimum(Function func, int dimensions, double rangeStart
     for (size_t i = 0; i < dimensions; ++i) {
         bestX[i] += SMALLEST_INCREMENT * (bestStep / divOffsets[i] % stepsPerDimension);
     }
-    std::cout << "Found the value " << bestValue << " for x = {";
-    for (double elem : bestX) {
-        std::cout << elem << ", ";
+    simpleOutput << bestValue << ",{";
+    for (size_t i = 0; i < bestX.size() - 1; ++i) {
+        simpleOutput << bestX[i] << ' ';
     }
-    std::cout << "\b\b} using the deterministic method\n\n";
+    simpleOutput << bestX.back() << "},deterministic,";
+    auto stop = high_resolution_clock::now();
+    simpleOutput << duration_cast<milliseconds>(stop - start).count();
+    return simpleOutput.str();
 }
 
 int main() {
-    std::cout << std::fixed << std::setprecision(5);
-    using namespace std::chrono;
-    auto start = high_resolution_clock::now();
-    euristicGlobalMinimum(Function(customFunc, "custom function"), 10, 0, 1);
-    auto stop = high_resolution_clock::now();
-    std::cout << "Function runtime was: " << duration_cast<seconds>(stop - start).count() << " seconds\n";
+    Function michalewiczFunc(michalewicz, "Michalewicz's function", 1, 3);
+    Function rastriginFunc(rastrigin, "Rastrigin's function", -1, 1);
+    Function sumSquaresFunc(shubert, "Sum Squares function", -1, 1);
+    Function sphereFunc(sphere, "Sphere function", -1, 1);
+    Function funcs[] = {
+        //michalewiczFunc,
+        rastriginFunc,
+        sumSquaresFunc,
+        //sphereFunc
+    };
+    std::ofstream fout("output2.csv", std::ios_base::app);
+    if (!fout.is_open()) {
+        std::cerr << "output file could not be created";
+        return 1;
+    }
+    constexpr int REPETITIONS = 5;
+    for (int _ = 0; _ < REPETITIONS; ++_) {
+        std::vector<std::future<std::string>> deterministicFutures;
+        for (const Function& f : funcs) {
+            deterministicFutures.emplace_back(std::async(std::launch::async, deterministicGlobalMinimum, f, 2));
+            deterministicFutures.emplace_back(std::async(std::launch::async, deterministicGlobalMinimum, f, 2));
+        }
+        while (deterministicFutures.front().wait_for(std::chrono::milliseconds(100)) != std::future_status::ready) {
+            std::vector<std::future<std::string>> heuristicFutures;
+            for (const Function& f : funcs) {
+                fout << heuristicGlobalMinimum(f, 2) << std::endl;
+                heuristicFutures.emplace_back(std::async(std::launch::async, heuristicGlobalMinimum, f, 10));
+            }
+            for (std::future<std::string>& ft : heuristicFutures) {
+                fout << ft.get() << std::endl;
+            }
+        }
+        for (std::future<std::string>& ft : deterministicFutures) {
+            fout << ft.get() << std::endl;
+        }
+    }
 }
